9877c5b37a83b689c45030e331855e072a020ad9
Mabel Tomety <mabeltomety18@gmail.com>
2018-07-14 16:35:12 -0700

    JSON

--- lesson-9/js/json.js
+++ lesson-9/js/json.js
@@ -3,13 +3,15 @@ var requestURL = 'https://api.openweathermap.org/data/2.5/weather?id=4156210&APP
 request.open('GET', requestURL);
 request.send();
 
-request.onload = function() {
-  var weatherinfo = JSON.parse(request.responseText);
-/*Here you will call the elements for example*/
+request.onload = function () {
+    var weatherinfo = JSON.parse(request.responseText);
+    /*Here you will call the elements for example*/
 
-document.getElementById('name').innerHTML = weatherinfo.name;
+    document.getElementById('name').innerHTML = weatherinfo.name;
     document.getElementById('wind').innerHTML = weatherinfo.wind.speed;
+    document.getElementById('temp').innerHTML = Math.round(weatherinfo.main.temp);
+    document.getElementById('weather').innerHTML = weatherinfo.weather;
+    document.getElementById('low').innerHTML = weatherinfo.main.temp_min;
+    document.getElementById('high').innerHTML = weatherinfo.main.temp_max;
+    document.getElementById('icon').innerHTML = weatherinfo.09d;
 }
-
-    
- 
